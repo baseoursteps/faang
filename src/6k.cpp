689e82ddec53c9dabd085ae1734a898514ddbcdb
​ <>
2021-05-06 20:59:09 +0300

    make kth more interesting

--- src/6k.cpp
+++ src/6k.cpp
@@ -13,14 +13,17 @@ main()
   std::srand(time(0));
   constexpr const auto kth{ 34 };
 
-  for (int i = -100; i < 100; i++) {
+  for (int i = 0; i < 100; i++) {
+    auto val = std::abs(rand()) % 1000;
+    std::cout << "val: " << val << "\n";
     auto repeat = std::abs(rand()) % 5;
-    for (int r = 0; r < 1; r++) {
+
+    for (int r = 0; r < repeat; r++) {
       if (vals.size() < kth - 1)
-        vals.push_back(i);
+        vals.push_back(val);
       else if (vals.size() == kth - 1) {
 
-        vals.push_back(i);
+        vals.push_back(val);
 
         // build heap from last leaf
         for (int i = vals.size() / 2 - 1; i >= 0; i--)
@@ -30,11 +33,11 @@ main()
       } else {
         // if the newest element is bigger than the root (kth largest) replace
         // and heapify
-        if (i > vals.front()) {
-          vals.at(0) = i;
+        if (val > vals.front()) {
+          vals.at(0) = val;
           heapify(vals, vals.size(), 0, false);
+          std::cout << kth << " max is: " << vals.front() << "\n";
         }
-        std::cout << kth << " max is: " << vals.front() << "\n";
       }
     }
   }

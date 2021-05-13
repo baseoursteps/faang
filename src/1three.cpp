1607ebf46da56fdca1c9faedeb4655e5ffdf08a1
​ <>
2021-05-13 13:11:39 +0300

    cleaned 1

--- src/1three.cpp
+++ src/1three.cpp
@@ -1,26 +1,18 @@
 #include <algorithm>
+#include <bits/stdint-intn.h>
 #include <cstdint>
 #include <cstdio>
 #include <map>
+#include <set>
 #include <vector>
 
 struct tpl
 {
-  std::map<size_t, std::int64_t> indeces;
   std::vector<int64_t> vals;
-
-  tpl(size_t i, size_t j, size_t k)
-  {
-    indeces.insert({ 0, i });
-    indeces.insert({ 1, j });
-    indeces.insert({ 2, k });
-  }
-
   tpl(size_t i, size_t j)
   {
-    indeces.insert({ 0, i });
-    indeces.insert({ 1, j });
-    indeces.insert({ 2, -1 });
+    vals.push_back(i);
+    vals.push_back(j);
   }
 };
 
@@ -34,14 +26,14 @@ main()
     nos.push_back(i);
 
   map<int64_t, tpl> sums;
-  map<int64_t, std::map<int64_t, int64_t>> vals;
+  set<vector<int64_t>> setty;
 
   for (size_t i = 0; i < nos.size() - 1; i++)
     for (size_t j = 1; j < nos.size(); j++) {
       if (i == j)
         continue;
 
-      sums.insert_or_assign(nos.at(i) + nos.at(j), tpl(i, j));
+      sums.insert({ nos.at(i) + nos.at(j), tpl(i, j) });
     }
 
   constexpr const int64_t x = 420;
@@ -49,25 +41,22 @@ main()
     auto&& rez = sums.find(x - nos.at(i));
 
     if (rez != sums.end()) {
-      auto j = rez->second.indeces.at(0);
-      auto k = rez->second.indeces.at(1);
-      if (i != j && i != k) {
-        rez->second.indeces.at(2) = i;
+      auto& items = rez->second.vals;
+      auto j = items.at(0);
+      auto k = items.at(1);
 
-        auto&& s = rez->second.vals;
-        s = { nos.at(i), nos.at(j), nos.at(k) };
-
-        sort(s.begin(), s.end());
-        vals.insert({ s.at(0), {} });
-        auto&& v = vals.at(s.at(0));
-        v.insert({ s.at(1), s.at(2) });
+      if (i != j && j != k) {
+        items.push_back(i);
+        setty.insert(items);
       }
     }
   }
 
-  for (auto&& i : vals)
-    for (auto&& [j, k] : i.second)
-      std::printf("found pair %ld %ld %ld \n", i.first, j, k);
+  std::printf("found pairs: \n");
+  for (auto&& i : setty) {
+    std::printf(
+      "\t%ld %ld %ld\n", nos.at(i.at(0)), nos.at(i.at(1)), nos.at(i.at(2)));
+  }
 
   return 0;
 }
